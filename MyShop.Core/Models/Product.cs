f955df331d04e9994ffb62ac1eb56b3e95962aae
olasubomi4 <40355148+olasubomi4@users.noreply.github.com>
2021-11-19 19:01:36 +0100

    done with this branch

--- MyShop.Core/Models/Product.cs
+++ MyShop.Core/Models/Product.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product :BaseEntity
     {
-        public string Id { get; set; }
+       
 
         [StringLength(20)]
         public string Name { get; set; }
@@ -18,11 +18,6 @@ namespace MyShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
 
     }
 }
