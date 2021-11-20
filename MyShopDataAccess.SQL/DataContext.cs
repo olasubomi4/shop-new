ebd87adb099f177134aa907201fa0a15d927ed8a
olasubomi <olasubomiodekunle@gmail.com>
2021-11-20 07:59:57 +0100

    Database is working now

---
+++ MyShopDataAccess.SQL/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataContext, Configuration>());
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    }
+}
